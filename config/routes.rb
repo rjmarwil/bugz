93f2319324f4f2b4e7b6145349b861095fdddab6
Jake Marwil <rjm02006@gmail.com>
2015-02-19 14:00:26 -0700

    change post signin route to sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "quotes#index"
   get "signin" => "sessions#new", as: :signin
-  post "signin" => "sessions#destroy"
+  post "signin" => "sessions#create"
   get "signout" => "sessions#destroy", as: :signout
 end
